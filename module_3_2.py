a59262653231dbb5f105da4a8edf6c54135106fa
Troy-nn <nrgnnrf@gmail.com>
2024-08-22 12:58:14 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,6 @@
 def send_email(message, recipient, sender ='university.help@gmail.com'):
 
-    if ("@" and (".com" or ".ru" or ".net")) not in (sender or recipient):
+    if ("@" or (".com" or ".ru" or ".net")) not in (sender and recipient):
         print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
     elif sender == recipient:
             print("Нельзя отправить письмо самому себе!")
@@ -10,9 +10,8 @@ def send_email(message, recipient, sender ='university.help@gmail.com'):
         print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
 
 send_email(1,  'money@gmail.com')
-
 send_email(1, 'urban.teacher@mail.uk', 'university.help@gail.com')
-send_email(1,'money@gmail.com', 'pochta@ru')
+send_email(1,'moneymail.com', 'university.help@gmail.com')
 send_email(1,'university.help@gmail.com', 'university.help@gmail.com' )
 
 
